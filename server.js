c83002e76e3a74d2f2d392e98d27240cdc900283
Ryan Fulmer <rfulmer20@gmail.com>
2020-05-09 13:08:25 -0400

    minor changes for deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${PORT}`);
 });
